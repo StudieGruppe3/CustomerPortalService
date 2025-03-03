c8a8c4c2aee8e085a98fee91314a75a9a130579d
vincentlorenzen <vincent.lorenzen@gmail.com>
2025-03-03 11:18:08 +0100

    Initial commit

---
+++ CustomerPortalServiceWeb.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CustomerPortalServiceWeb.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
